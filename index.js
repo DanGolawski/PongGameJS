45020304e61f476cc1643aa9467d52e70bfe7b8e
Daniel Golawski <dangolawski@gmail.com>
2020-09-26 21:48:44 +0200

    js practice

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 window.onload = () => {
+
     let canvas = document.querySelector('#myCanvas');
     let ctx = canvas.getContext('2d');
+    let x = canvas.width / 2;
+    let y = canvas.height - 30;
+    let dx = 1;
+    let dy = -1;
 
-    ctx.beginPath();
-    ctx.rect(20, 20, 50, 50);
-    ctx.fillStyle = '#FF0000';
-    ctx.fill();
-    ctx.closePath();
+    function drawBall() {
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, Math.PI * 2);
+        ctx.fillStyle = '#0095DD';
+        ctx.fill();
+        ctx.closePath();
+    }
 
-    ctx.beginPath();
-    ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-    ctx.fillStyle = 'green';
-    ctx.fill();
-    ctx.closePath();
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBall();
+        x += dx;
+        y += dy;
+    }
 
-    ctx.beginPath()
-    ctx.rect(160, 10, 100, 40);
-    ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-    ctx.stroke();
-    ctx.closePath();
+    setInterval(draw, 10);
 }
+
+// https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Bounce_off_the_walls
